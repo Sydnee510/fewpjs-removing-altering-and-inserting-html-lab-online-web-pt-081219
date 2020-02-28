3f73fb04178d52722887487197904f026e9d466c
sydnee510 <sbsydnee161416@gmail.com>
2020-02-28 01:39:12 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove("main");
+// element.newHeader = "victory"; 
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Sydnee is the champion"; 
